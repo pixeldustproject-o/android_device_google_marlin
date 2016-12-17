218ec754a1732cc5cdb2a494094f4c6471c39058
beanstown106 <nbenis106@gmail.com>
2016-12-16 23:09:34 -0500

    common: add fingerprint sensor type prop
    Change-Id: I45895b2a649f7d2c21a0fa5f89c8157516b0fbb3
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- device-common.mk
+++ device-common.mk
@@ -638,6 +638,10 @@ PRODUCT_PACKAGES += \
     libunwind.vndk-sp\
     liblzma.vndk-sp\
 
+# Fingerprint sensor type
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.fingerprint=fpc
+
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
