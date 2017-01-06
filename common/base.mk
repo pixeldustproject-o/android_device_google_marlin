dca0b518f605b63a44e1fe18df3f6eb4893f188c
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-05 19:27:30 -0600

    common: disable bt testing/debugging apps
    Change-Id: I16b98c9a2bf27b1f515eee1c350928dc9cefc5eb
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- common/base.mk
+++ common/base.mk
@@ -668,11 +668,7 @@ PRODUCT_PACKAGES := \
 ifneq ($(TARGET_USES_AOSP),true)
 PRODUCT_PACKAGES += \
        BluetoothExt \
-       BTTestApp \
-       HiddTestApp \
-       BTLogKit \
        a4wpservice \
-       BTLogSave \
        wipowerservice
 endif
 
