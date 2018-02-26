e5b6392064f951e75fec235e51a629b62e662d97
bmc08gt <brandon.mcansh@gmail.com>
2018-02-26 00:05:30 -0800

    marlin/sailfish: Update dalvik heap amd hwui overrides
    Change-Id: I4106ce7d6ccc8a8fde6fe0e974dd872a4a7de8f8
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- device-common.mk
+++ device-common.mk
@@ -43,9 +43,17 @@ PRODUCT_COPY_FILES += \
 
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapminfree=2m \
     ro.telephony.default_cdma_sub=0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=384m \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=288m \
+    dalvik.vm.heapsize=768m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, device/google/marlin/common/common64.mk)
@@ -77,14 +85,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # HWUI common settings
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.gradient_cache_size=2 \
+    ro.hwui.drop_shadow_cache_size=8 \
     ro.hwui.r_buffer_cache_size=8 \
     ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=2048
+    ro.hwui.texture_cache_size=88 \
+    ro.hwui.layer_cache_size=58 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.shape_cache_size=4 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_large_cache_width=4096 \
+    ro.hwui.text_large_cache_height=4096
 
 # enable FIFO UI scheduling by default
 PRODUCT_PROPERTY_OVERRIDES += \
